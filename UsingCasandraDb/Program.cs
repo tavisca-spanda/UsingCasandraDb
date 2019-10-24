9185a737b1bf80c6af1edf14cffc0e32de024801
spanda <spanda@tavisca.in>
2019-10-24 12:31:09 +0530

    Working propoerly

--- UsingCasandraDb/Program.cs
+++ UsingCasandraDb/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Cassandra;
 
 namespace UsingCasandraDb
 {
@@ -6,7 +7,23 @@ namespace UsingCasandraDb
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cluster = Cluster.Builder()
+                     .AddContactPoints("localhost")
+                 
+                     .Build();
+            
+            var session = cluster.Connect("videodb");
+
+            //var videoStatement = session.Prepare("INSERT INTO videos (videoid, videoname, username) values (?,?,?)");
+            //var batch = new BatchStatement()
+            //            .Add(videoStatement.Bind(new Guid ("b3a76c6b-7c7f-4af6-964f-803a9283c401"),"xyz","abc"));
+            //session.Execute(batch);
+            var rs = session.Execute("SELECT * FROM videos");
+            foreach (var row in rs)
+            {
+                Console.WriteLine(row.GetValue<string>(1));
+            }
+            Console.ReadKey();
         }
     }
 }
